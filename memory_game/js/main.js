b04f4ee43dc203b3fcdcb1330365598b7ba492ee
friedneckbones <malissastrong@yahoo.com>
2018-02-17 01:12:32 -0800

    pushtwo

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,15 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push("cardOne", "cardTwo");
+console.log("User flipped queen");
+var cardTwo = cards[2];
+console.log("User flipped king");
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[2]) {
+    alert("You found a match!");
+}
+else {
+  alert("Sorry, try again");
+}
+}
