4fd2a5f062d336a1328ea20d79a3d2234c7463bc
friedneckbones <malissastrong@yahoo.com>
2018-02-19 15:27:45 -0800

    (pushfour)

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds"
+  }
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -11,8 +32,10 @@ else {
 };
 
 var flipCard = function(cardId) {
-  console.log ("User fliped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
   if (cardsInPlay.length === 2) {
   checkForMatch ();
 }
@@ -20,8 +43,3 @@ var flipCard = function(cardId) {
 
   flipCard (0);
   flipCard (2);
-
-
-
-}
-};
